d1f494f98dfd936dbc5c6597f6df815eff6b48ac
gmac <gmacwill77@gmail.com>
2014-01-04 14:54:00 -0500

    Updating grunt build process.

---
+++ gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+	  pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+				banner: '// mmd.js <%= pkg.version %>\n// (c) 2011-2014 Greg MacWilliam\n// Freely distributed under the MIT license\n// Docs: github.com/gmac/micro-module-definition\n'
+			},
+			root: {
+				src: '<%= pkg.name %>.js',
+				dest: '<%= pkg.name %>.min.js',
+				sourceMapUrl: '<%= pkg.name %>.min.map',
+				sourceMapRoot: './'
+			}
+		}
+	});
+  
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.registerTask('default', ['uglify']);
+};
